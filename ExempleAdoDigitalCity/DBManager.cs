f16d83dd962e56fdc49106be034607578c669eb3
michael person <michael.person@cognitic.be>
2025-09-15 15:52:21 +0200

    Début du getAllJokes

--- ExempleAdoDigitalCity/DBManager.cs
+++ ExempleAdoDigitalCity/DBManager.cs
@@ -69,5 +69,73 @@ namespace ExempleAdoDigitalCity
                 }
             return true;
         }
+
+        public List<Jokes> GetAllJokes()
+        {
+            DbConnection connexion = new SqlConnection(connectionString);
+            try
+            {
+
+                //Appeler la méthode qui permettra de se connecter
+                //et Vérifier qu'on est connecte
+                connexion.Open();
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"Erreru de connexion db : {ex.Message}");
+            }
+
+
+            if (connexion.State == System.Data.ConnectionState.Open)
+            {
+
+                //Exécuter une requête select
+                //string sqlQuery = @"GetJokes";
+                //DbCommand command = connexion.CreateCommand();
+                //command.CommandText = sqlQuery;
+                //command.CommandType = System.Data.CommandType.StoredProcedure;
+                //command.Parameters.Add(new SqlParameter("motcle", "vache"));
+
+                try
+                {
+                    string sqlQuery = @"Select Id, Title, Body 
+                        From Jokes";
+                    DbCommand command = connexion.CreateCommand();
+                    command.CommandText = sqlQuery;
+                    //Afficher le résultat de ma requête
+                    DbDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        int? id = reader["Id"] == DBNull.Value ? null : (int?)reader["Id"];
+                        string? titre = reader["Title"] == DBNull.Value ? "" : reader["Title"].ToString();
+                        Jokes j = new Jokes();
+                        j.Id = id.Value;
+                        j.Title = titre;
+                        j.Body = reader["Body"] == DBNull.Value ? "" : reader["Body"].ToString(); ;
+                        Console.WriteLine(titre);
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+
+            }
+
+            //je ferme
+            try
+            {
+
+
+                connexion.Close();
+            }
+            catch (SqlException sqex)
+            {
+                Console.WriteLine(sqex.Message);
+            }
+        }
     }
 }
